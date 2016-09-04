0a84e0b5efcc108936d55ab01921a2096aa0dc1d
Anna Langer <Anna Langer>
2016-09-04 13:11:12 +0200

    Load data from data folder

---
+++ LoadData.R
@@ -0,0 +1,11 @@
+#     Titanic Load Data Script
+#
+#     04.09.2016 
+
+
+test <- read.csv("./data/test.csv")
+train <- read.csv("./data/train.csv")
+
+gendermodel <- read.csv("./data/gendermodel.csv")
+
+genderclassmodel <- read.csv("./data/genderclassmodel.csv")
