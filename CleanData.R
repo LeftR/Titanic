71fd58301180ac663195d7190391661c512abf0a
LeftR <cyklista@gmail.com>
2016-09-16 12:45:56 +0200

    1sth attempt

--- CleanData.R
+++ CleanData.R
@@ -6,7 +6,7 @@ test$DataSetName <- "Test"
 
 train$DataSetName <- "Train"
 
-### Connect trai and test data 
+### Connect train and test data 
 full <- rbind(train, test)
 
 full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
@@ -36,9 +36,60 @@ prop.table(table(full$Sex, full$Title),1)
 
 
 full$Surname <- gsub('(\\,.*)', "", full$Name)
-train$Surname <- gsub('(\\,.*)', "", train$Name)
 
-Surename <- train[c("Surname","Ticket","")]
 
-Surename.group <- Surename %>% group_by("Ticket")
+Surename <- full[c("Surname","Ticket")]
 
+Surename.group <- Surename %>% group_by("Ticket") 
+
+
+
+
+# Get rid of our missing passenger IDs
+embark_fare <- full %>%
+  filter(PassengerId != 62 & PassengerId != 830)
+
+# Use ggplot2 to visualize embarkment, passenger class, & median fare
+ggplot(embark_fare, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
+  geom_boxplot() +
+  geom_hline(aes(yintercept=80), 
+             colour='red', linetype='dashed', lwd=2) +
+  scale_y_continuous(labels=dollar_format()) +
+  theme_few()
+full$Embarked[c(62, 830)] <- 'C'
+
+ggplot(full[full$Pclass == '3' & full$Embarked == 'S', ], 
+       aes(x = Fare)) +
+  geom_density(fill = '#99d6ff', alpha=0.4) + 
+  geom_vline(aes(xintercept=median(Fare, na.rm=T)),
+             colour='red', linetype='dashed', lwd=1) +
+  scale_x_continuous(labels=dollar_format()) +
+  theme_few()
+full$Fare[1044] <- median(full[full$Pclass == '3' & full$Embarked == 'S', ]$Fare, na.rm = TRUE)
+
+
+# Make variables factors into factors
+factor_vars <- c('PassengerId','Pclass','Sex','Embarked',
+                 'Title','Surname')
+
+full[factor_vars] <- lapply(full[factor_vars], function(x) as.factor(x))
+
+# Set a random seed
+set.seed(129)
+
+# Perform mice imputation, excluding certain less-than-useful variables:
+mice_mod <- mice(full[, !names(full) %in% c('PassengerId','Name','Ticket','Cabin','Surname','Survived')], method='rf') 
+mice_output <- complete(mice_mod)
+
+# Plot age distributions
+par(mfrow=c(1,2))
+hist(full$Age, freq=F, main='Age: Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$Age, freq=F, main='Age: MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+# Replace Age variable from the mice model.
+full$Age <- mice_output$Age
+
+# Show new number of missing Age values
+sum(is.na(full$Age))
--- Titanic.R
+++ Titanic.R
@@ -1,3 +1,8 @@
 library("dplyr")
 source("LoadData.R")
 source("CleanData.R")
+library('randomForest')
+library('ggplot2') # visualization
+library('ggthemes') # visualization
+library('scales') # visualization
+library('mice')
